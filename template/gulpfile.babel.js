d7076eba640f322681a5ba5f91129732fbcb22ab
litor <547794529@qq.com>
2017-05-24 15:55:54 +0800

    first commit

--- template/gulpfile.babel.js
+++ template/gulpfile.babel.js
@@ -1,9 +1,5 @@
-import ubase from 'ubase-vue';
+import ubase from 'ubase-vue'
 
 ubase({
-  // 输出目录
-  dest: './www',
-
-  // 端口
-  port: '8081'
-});
+  port: '8082'
+})
---
+++ template/src/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      name:{{index.name}}
+    </div>
+</template>
+<script>
+    export default {
+      computed: {
+        ...Vuex.mapState({
+          index: state => state.index
+        }),
+      }
+    }
+</script>
